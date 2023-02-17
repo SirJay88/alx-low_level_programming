77c86a09c1eb9ef9e628262489c01336416d3dbc
SirJay <moledijustice@gmai.com>
2023-02-16 18:21:20 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-	wrie(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	system("echo 'and that piece of art is useful\" \ - Dora Korpar, 2015-10-19'");
+
 	return (1);
 }
