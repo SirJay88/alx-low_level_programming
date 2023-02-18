fa0c92e357cc66e823ec480388c3771e769fc24f
SirJay <moledijustice@gmai.com>
2023-02-17 20:35:38 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,18 +5,17 @@
  * Return: 0
  */
 int main(void)
-
 {
-        char ch = 'a';
+	char ch = 'a';
 
-        while (ch <= 'z')
-        {
-                if ((ch != 'e') && (ch != 'q'))
-                {
-                        putchar(ch);
-                }
-                ch++;
-        }
-        putchar('\n');
-        return (0);
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
