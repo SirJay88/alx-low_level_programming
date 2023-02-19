26c66db49c529a09597366aac2f18c9761650f84
SirJay <moledijustice@gmai.com>
2023-02-18 16:20:35 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,14 +15,13 @@ int main(void)
 	{
 		putchar(i % 10 + '0');
 		putchar(j % 10 + '0');
-
+		}
 		if (i != 8)
-			(j != 9);
 			{
 		putchar(',');
 		putchar(' ');
 		}
-	}
+
 	}
 	putchar('\n');
 	return (0);
