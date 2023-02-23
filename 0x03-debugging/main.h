74b8ba3b4b4cade17823e0cfc02da5966874a043
SirJay <moledijustice@gmai.com>
2023-02-23 07:46:40 -0800

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 void positive_or_negative(int i);
+int conert_number(int a, int b, int c);
 int largest_number(int a, int b, int c);
+int print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
