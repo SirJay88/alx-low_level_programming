54cfa28ce1b94d32aeb985df64afa498be345fd7
SirJay88 <moledijustice@gmail.com>
2023-03-22 17:34:27 -0700

    0-print_name

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name_as_is - prints a name as is
@@ -19,22 +20,22 @@ void print_name_as_is(char *name)
  */
 void print_name_uppercase(char *name)
 {
-	unsigned int i;
+    unsigned int i;
 
-	printf("Hello, my uppercase name is ");
-	i = 0;
-	while (name[i])
-	{
-	if (name[i] >= 'a' && name[i] <= 'z')
+    printf("Hello, my uppercase name is ");
+    i = 0;
+    while (name[i])
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
         {
-	putchar(name[i] + 'A' - 'a');
-	}
-	else
-	{
-	putchar(name[i]);
-	}
-	i++;
-	}
+            putchar(name[i] + 'A' - 'a');
+        }
+        else
+        {
+            putchar(name[i]);
+        }
+        i++;
+    }
 }
 
 /**
@@ -44,8 +45,8 @@ void print_name_uppercase(char *name)
  */
 int main(void)
 {
-	print_name("Bob", print_name_as_is);
-	print_name("Bob Dylan", print_name_uppercase);
-	printf("\n");
-	return (0);
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    printf("\n");
+    return (0);
 }
--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
+#include "function_pointers.h"
 #include <stdio.h>
-
 /**
  * print_name - print name using pointer to function
  * @name: string to add
@@ -7,8 +7,9 @@
  * Return: nothing
  **/
 void print_name(char *name, void (*f)(char *))
-{
+	{
 	if (name == NULL || f == NULL)
-		return;
+	return;
+
 	f(name);
 }
